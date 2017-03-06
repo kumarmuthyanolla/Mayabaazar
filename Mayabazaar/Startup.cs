540c6ed326f6a24c0e0b51b617734063cc1ecb09
chait <chait@DESKTOP-UAT8K1V>
2017-03-06 01:31:46 -0500

    Add project files.

---
+++ Mayabazaar/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mayabazaar.Startup))]
+namespace Mayabazaar
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
